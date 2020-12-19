5e516e3203e1234e9e910fdce02d1413ebc6fc82
wwwsergey <53620617+wwwsergey@users.noreply.github.com>
2020-12-18 21:33:59 -0600

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,10 @@
+package com.tutorials;
+
+@Author(name="Sergey")
+public class TestAuthor 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Don't I look good??");
+    }
+}
